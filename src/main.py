4cd44f85b19cfaae55c018637ea6826d6580528c
Charles Akinnurun <charlesakinnurun@gmail.com>
2026-09-04 12:49:25 +0100

    ADD: main py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from pyfiglet import figlet_format
+from termcolor import colored
+
+print(colored(figlet_format("JANMASHTAMI",
+                            font="silent"), "yellow"))
+
+print(colored("Jai Shree Krishna", "cyan"))
